6837bd4c96f58f48eb1b42729e632df413554a76
dermio <thisdjay@gmail.com>
2017-10-29 12:07:33 -0700

    Finished it block to test for `buzz`.

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -4,11 +4,20 @@ chai.should();
 const fizzBuzzer = require("../fizzBuzzer");
 
 describe("fizzBuzzer", function () {
-  it("should return fizz-buzz when divisible by 15", function () {
+  // test for normal cases
+  it("should return `fizz-buzz` when divisible by 15", function () {
     let normalCases = [15, 60, 120];
     normalCases.forEach(function (input) {
       let answer = fizzBuzzer(input);
       answer.should.equal("fizz-buzz");
+    });
+  });
+
+  it("should return `buzz` when divisible by 5", function () {
+    let normalCases = [10, 50, 100];
+    normalCases.forEach(function (input) {
+      let answer = fizzBuzzer(input);
+      answer.should.equal("buzz");
     });
   });
 
